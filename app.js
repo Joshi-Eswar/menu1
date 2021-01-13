199c233d0883e9fc122d7102e7473b4dd1bbebf8
Joshi-Eswar <65666329+Joshi-Eswar@users.noreply.github.com>
2021-01-13 17:47:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,348 @@
+const menu = [
+  ,
+  {
+    id: 11,
+    title: "Sheekh Kabab",
+    category: "Starter",
+    price: 250,
+    img: "https://foodiesterminal.com/wp-content/uploads/2019/11/chicken-angara-kabab-2-679x1024.jpg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 12,
+    title: "Chicken 65",
+    category: "Starter",
+    price: 300,
+    img: "https://i1.wp.com/swatisani.net/kitchen/wp-content/uploads/2014/11/1200px-Chicken_65_Dish.jpg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 13,
+    title: "Tandoori Chicken",
+    category: "Starter",
+    price: 500,
+    img: "https://www.cubesnjuliennes.com/wp-content/uploads/2019/11/Tandoori-Chicken-1.jpg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 14,
+    title: "Chicken Liver Fry",
+    category: "Starter",
+    price: 250,
+    img: "https://2.bp.blogspot.com/-9S3vR6YAkaM/WNerser999I/AAAAAAAAHQQ/qq76lUs2EysaB-8elZE_L3kljwHtgdNHwCLcB/s1600/000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000A00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.jpg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 15,
+    title: "Thali Breakfast",
+    category: "breakfast",
+    price: 450,
+    img: "https://lh5.googleusercontent.com/proxy/NdZ6hhFt0Ab48EChlbSR4-jBmMW807Qcen1i2oSLCOveudDyelKz8s6eXdJdcOQKy2DffasZtUVQHa67qzoNoAPw4XATSiIsalyyjFADvbl4kpY=w1200-h630-p-k-no-nu",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },{
+    id: 16,
+    title: "mini tiffen",
+    category: "breakfast",
+    price: 150,
+    img: "https://thumbs.dreamstime.com/z/south-indian-breakfast-idli-dosa-chutney-indian-breakfast-lentil-rice-pancake-also-known-as-dosa-dumplings-known-as-idli-158574513.jpg",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },{
+    id: 17,
+    title: "Plain Dosa",
+    category: "breakfast",
+    price: 100,
+    img: "https://www.hungryforever.com/wp-content/uploads/2015/05/dosa.jpg",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },{
+    id: 18,
+    title: "Idli",
+    category: "breakfast",
+    price: 100,
+    img: "https://thumbs.dreamstime.com/z/south-indian-breakfast-south-indian-breakfast-idli-sambhar-145012253.jpg",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },{
+    id: 19,
+    title: "Puri",
+    category: "breakfast",
+    price: 450,
+    img: "https://media.istockphoto.com/photos/sujisooji-halwa-puri-or-shira-poori-with-black-chana-masala-breakfast-picture-id1049749112?k=6&m=1049749112&s=612x612&w=0&h=u-ndDEk_hf1zwd7JMEgyY2BycM8QNDd8ePbddkfq-F4=",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 20,
+    title: "veg Meals",
+    category: "lunch",
+    price: 350,
+    img: "https://www.hlimg.com/images/stories/738X538/cover-pic_1527239659m.jpg",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 21,
+    title: "non-veg Meals",
+    category: "lunch",
+    price: 500,
+    img: "https://feenix.co.in/wp-content/uploads/2017/12/nonveg-meals.jpg",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 22,
+    title: "North-Indian Meals",
+    category: "lunch",
+    price: 250,
+    img: "https://www.subbuskitchen.com/wp-content/uploads/2014/07/NorthIndian-Lunch-Menu1_Final2.jpg",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 23,
+    title: "Nan-Curry",
+    category: "lunch",
+    price: 150,
+    img: "https://www.pepperdelight.com/wp-content/uploads/2018/08/pepper-delight-matter-paneer-2-500x500.jpg",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 24,
+    title: "Parota-curry",
+    category: "lunch",
+    price: 150,
+    img: "https://3.bp.blogspot.com/-cBV765nFQSg/Wbev1Bvh2rI/AAAAAAAACxs/6lHRWI5c0dcdP1sBJHFnWBGQi4FbR3C2ACLcBGAs/s1600/21.PNG",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 25,
+    title: "mango milkshake",
+    category: "shakes",
+    price: 100,
+    img: "https://www.elgiultra.com/ultraliving/wp-content/uploads/2019/10/mango-milkshake.jpg",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 26,
+    title: "dryfruit milkshake",
+    category: "shakes",
+    price: 150,
+    img: "https://ratlamee.com/wp-content/uploads/2018/11/Dry-Fruit-Milk-Shake.jpg",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 27,
+    title: "Banana milkshake",
+    category: "shakes",
+    price: 100,
+    img: "https://butterwithasideofbread.com/wp-content/uploads/2016/08/Easy-Banana-Split-Milkshakes.BSB_.IMG_0510.jpg",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 28,
+    title: "orange milkshake",
+    category: "shakes",
+    price: 150,
+    img: "https://www.simplehealthykitchen.com/wp-content/uploads/2014/12/stress-buster-orange-smoothie-simplehealthykitchen.com_.jpg",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 29,
+    title: "Butterfruit milkshake",
+    category: "shakes",
+    price: 150,
+    img: "https://b.zmtcdn.com/data/pictures/chains/1/19077631/379a997d4c37444d9802df7194789025.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 30,
+    title: "Meals",
+    category: "dinner",
+    price: 350,
+    img: "https://image.shutterstock.com/image-photo/indian-hindu-veg-thali-known-260nw-345624869.jpg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 31,
+    title: "Sambar and Rice",
+    category: "dinner",
+    price: 100,
+    img: "https://thumbs.dreamstime.com/b/rice-sambar-popular-south-indian-households-its-very-popular-savory-dish-south-indian-cuisine-41417916.jpg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 32,
+    title: "curd Rice",
+    category: "dinner",
+    price: 100,
+    img: "https://static.toiimg.com/thumb/75732038.cms?imgsize=1313318&width=800&height=800",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 33,
+    title: "Chapathi and Curry",
+    category: "dinner",
+    price: 100,
+    img: "https://image.shutterstock.com/image-photo/traditional-paneer-sabji-masala-chapathi-260nw-1034447119.jpg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 34,
+    title: "Mutton Biriyani",
+    category: "dinner",
+    price: 300,
+    img: "https://vaya.in/recipes/wp-content/uploads/2018/03/Ambur-Chicken-Biriyani.jpg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 1,
+    title: "buttermilk pancakes",
+    category: "breakfast",
+    price: 450,
+    img: "./images/item-1.jpeg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },
+  {
+    id: 2,
+    title: "diner double",
+    category: "lunch",
+    price: 300,
+    img: "./images/item-2.jpeg",
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+  },
+  {
+    id: 3,
+    title: "godzilla milkshake",
+    category: "shakes",
+    price: 150,
+    img: "./images/item-3.jpeg",
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: "country delight",
+    category: "breakfast",
+    price: 250,
+    img: "./images/item-4.jpeg",
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5,
+    title: "egg attack",
+    category: "lunch",
+    price: 350,
+    img: "./images/item-5.jpeg",
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6,
+    title: "oreo dream",
+    category: "shakes",
+    price: 200,
+    img: "./images/item-6.jpeg",
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7,
+    title: "bacon overflow",
+    category: "breakfast",
+    price: 450,
+    img: "./images/item-7.jpeg",
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: "american classic",
+    category: "lunch",
+    price: 400,
+    img: "./images/item-8.jpeg",
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+  },
+  {
+    id: 9,
+    title: "quarantine buddy",
+    category: "shakes",
+    price: 200,
+    img: "./images/item-9.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+  {
+    id: 10,
+    title: "Mutton Curry",
+    category: "dinner",
+    price: 300,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  }
+  
+];
+// get parent element
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItems(menu);
+  buttonPlay();
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+   
+
+    return `<article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">₹${item.price}</h4>
+            </header>
+            <p class="item-text">
+              ${item.desc}
+            </p>
+          </div>
+        </article>`;
+  });
+ 
+  displayMenu = displayMenu.join("");
+
+
+  sectionCenter.innerHTML = displayMenu;
+}
+function buttonPlay(){
+  const categories=menu.reduce(function(value,item){
+    console.log(value);
+    console.log(item);
+    
+     if(!value.includes(item.category)){
+       value.push(item.category)
+   }return value},["all"]
+   )
+  
+
+   const catagoryBtns=categories.map(function(cat){
+     return `<button type="button" class="filter-btn" data-id=${cat}>
+                ${cat}
+           </button>`;
+   }).join("");
+   btnContainer.innerHTML=catagoryBtns;  
+
+
+const filterBtn=btnContainer.querySelectorAll(".filter-btn");
+
+filterBtn.forEach(function (btn) {
+  btn.addEventListener("click",function(e){
+    const cat1 =e.currentTarget.dataset.id;
+    const menuCat=menu.filter(function(menus){
+      if(menus.category == cat1){
+        return menus;
+      }
+
+    });
+    if(cat1==="all"){
+      displayMenuItems(menu);
+    }
+    else{
+      displayMenuItems(menuCat);
+    }
+
+  });
+});
+}
+
+
+
+
